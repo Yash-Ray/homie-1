4cf08cd9f84e4e325d2a28725464282383b74f42
@maverick0701 <madhavanandcp070@gmail.com>
2021-04-09 12:41:03 +0530

    first

--- controller/index.js
+++ controller/index.js
@@ -13,10 +13,11 @@ module.exports.index = async (req, res) => {
 const capitalizeFirstLetter = (string) =>
   string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 module.exports.getMood = async (req, res) => {
-  let { mood: mood } = req.body;
-  mood = capitalizeFirstLetter(mood);
-  let playlist = await Music.findOne({ mood: mood });
-  return res.json(200, {
-    playlist: playlist,
-  });
+  // let { mood: mood } = req.body;
+  // mood = capitalizeFirstLetter(mood);
+  // let playlist = await Music.findOne({ mood: mood });
+  // return res.json(200, {
+  //   playlist: playlist,
+  // });
+  return res.send("welcome");
 };
