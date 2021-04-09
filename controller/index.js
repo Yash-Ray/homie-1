898d7ddeee32230eebbe4b90ae7da9cee12cb093
@maverick0701 <madhavanandcp070@gmail.com>
2021-04-09 12:48:33 +0530

    second

--- controller/index.js
+++ controller/index.js
@@ -13,11 +13,10 @@ module.exports.index = async (req, res) => {
 const capitalizeFirstLetter = (string) =>
   string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 module.exports.getMood = async (req, res) => {
-  // let { mood: mood } = req.body;
-  // mood = capitalizeFirstLetter(mood);
-  // let playlist = await Music.findOne({ mood: mood });
-  // return res.json(200, {
-  //   playlist: playlist,
-  // });
-  return res.send("welcome");
+  let { mood: mood } = req.body;
+  mood = capitalizeFirstLetter(mood);
+  let playlist = await Music.findOne({ mood: mood });
+  return res.json(200, {
+    playlist: playlist,
+  });
 };
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const db = require("./config/mongoose");
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 app.use(express.urlencoded());
 
 app.use(cookieParser());
